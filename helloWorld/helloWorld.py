7799b2b032e0c95e2e97daf052430ba944f6cea2
Robbie Prokop <robbieprokop@gmail.com>
2022-12-02 16:34:08 +0400

    more print, data types, concat,

--- helloWorld/helloWorld.py
+++ helloWorld/helloWorld.py
@@ -1,5 +1,31 @@
-print('Hello World!')
-print(1 + 2)
-print(7 * 6)
-print()
-print("The End", "or is it?", "another print", 2+4)
+# print('Hello World!')
+# print(1 + 2)
+# print(7 * 6)
+# print()
+# print("The End", "or is it?", "another print", 2+4)
+
+greeting = "Hello"
+name = "Robbie"
+# name = input("Please enter your name ")
+
+# print(greeting, name)
+
+# splitString = "This string have been \nsplit over \nseveral\nlines"
+# print(splitString)
+
+# tabbedString = "1\t2\t3\t4\t5"
+# print(tabbedString)
+
+# print('quote\'s being backslashed')
+# # backslashed
+# print("""quote\'s being "backslashed" """)
+
+# print(type(greeting))
+age = 28
+# print(type(age))
+
+# # age = "2 years"
+# print("age",age)
+# print(type(age))
+
+print(name + " is " + age + " years old")
