f6616492c46d0bd0d6e7487687b724e0fc8ef712
Robbie Prokop <robbieprokop@gmail.com>
2022-12-02 16:55:32 +0400

    operators

--- helloWorld/helloWorld.py
+++ helloWorld/helloWorld.py
@@ -1,15 +1,18 @@
+# Print
 # print('Hello World!')
 # print(1 + 2)
 # print(7 * 6)
 # print()
 # print("The End", "or is it?", "another print", 2+4)
 
-greeting = "Hello"
-name = "Robbie"
+# Variables
+# greeting = "Hello"
+# name = "Robbie"
 # name = input("Please enter your name ")
 
 # print(greeting, name)
 
+# Escaped Characters
 # splitString = "This string have been \nsplit over \nseveral\nlines"
 # print(splitString)
 
@@ -20,12 +23,30 @@ name = "Robbie"
 # # backslashed
 # print("""quote\'s being "backslashed" """)
 
+# TYPE OF 
 # print(type(greeting))
-age = 28
+# age = 28.67
 # print(type(age))
 
 # # age = "2 years"
 # print("age",age)
 # print(type(age))
 
-print(name + " is " + age + " years old")
+# Cannot concat different data types
+# print(name + " is " + age + " years old")
+
+# Operators
+# a=12 
+# b=3
+# print(a/b) #returns a float
+# print(a//b) #returns an integer
+# print(a%b) #remainder after integer division
+
+# for i in range(1, 4): #start - end. start is included, end is not
+#   print(i)
+  
+# for i in range(1, a/b): #float cannot be interpreted as int
+#   print(i)
+# for i in range(1, a//b): #successfull operation
+#   print(i)
+
