04cb02f4a1665e6772ab65d5331e5adc3b453651
Robbie Prokop <robbieprokop@gmail.com>
2022-12-02 19:11:31 +0400

    simple concatenation

--- crashCourse/strings.py
+++ crashCourse/strings.py
@@ -1,5 +1,15 @@
 # Strings in python are surrounded by either single or double quotation marks. Let's look at string formatting and some string methods
 
+name = "Robbie"
+age = 28
+
+# Concat
+print("Hello, my name is " + name)
+print("Hello, my name is {0}".format(name))
+print("Hello, my name is " + name + " and I am " + str(age))
+print("Hello, my name is {0} and I am {1}".format(name, age))
+print(f"Hello, my name is {name} and I am {age}")
+
 
 # String Formatting
 
