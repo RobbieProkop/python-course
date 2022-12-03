96b8eed2a043c3733abc8dfd905a8b14e06e544a
Robbie Prokop <robbieprokop@gmail.com>
2022-12-03 16:09:45 +0400

    turn dict into JSON and reverse

--- crashCourse/py_json.py
+++ crashCourse/py_json.py
@@ -1,1 +1,20 @@
 # JSON is commonly used with data APIS. Here how we can parse JSON into a Python dictionary
+
+import json
+
+# sample JSON (pretend this comes from some API)
+userJSON = '{"first_name": "Robbie","last_name": "Prokop", "age": 28}'
+
+# parse to dict
+user = json.loads(userJSON)
+
+
+# print(user)
+# print(user['first_name'])
+
+# Turn DIctionary into JSOn
+car = {'make': 'Ford', 'model': 'GT', 'year': 1967}
+
+carJSON = json.dumps(car)
+
+print(carJSON)
