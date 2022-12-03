963084a4c2423db057ed68c8419c04a174a29202
Robbie Prokop <robbieprokop@gmail.com>
2022-12-03 16:04:28 +0400

    files

--- crashCourse/files.py
+++ crashCourse/files.py
@@ -1,1 +1,26 @@
 # Python has functions for creating, reading, updating, and deleting files.
+
+# open a file
+myFile = open('myfile.txt', 'w')
+
+# get info on file
+print('Name: ', myFile.name)
+print('is closed: ', myFile.closed)
+print('Mode: ', myFile.mode)
+
+# Write to file
+myFile.write("I love programming")
+myFile.write(' and hacking')
+myFile.close()
+
+#  append to file
+myFile = open('myfile.txt', 'a')
+
+myFile.write(" Appppeeennndedddd")
+myFile.close()
+
+
+# read from file
+myFile = open('myfile.txt', 'r+')
+text = myFile.read(100)
+print(text)
